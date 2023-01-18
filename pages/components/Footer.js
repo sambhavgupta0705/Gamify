5ef4a23f8199b288820d1801ea0c7fd09b2dd2bb
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-01-18 10:19:24 +0530

    added footer and enchanced the UI

---
+++ pages/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div>
+            <br/>
+            <h4 className="text-center m-auto  bg-gray-900">Made with <span className='text-red-500'>  :love:</span> by <a> 
+                Sambhav Gupta</a></h4>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,11 +1,13 @@
 import GithubCorner from "react-github-corner";
-
+import Footer from "./Footer";
 const Layout=({children})=>{
+
     return (
         <div className="content">
 
         <GithubCorner href="https://github.com/sambhavgupta0705/Gamify" target="_blank"/>
         {children}
+        <Footer/>
         </div>
     );
 
